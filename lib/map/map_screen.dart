244713da80c84d26a5b3cea2fe5497bf0f653d25
Jaehyun9912 <open305045@gmail.com>
2023-03-09 16:01:52 +0900

    code setting
    remove linux

---
+++ lib/map/map_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+  @override
+  _MapScreen createState() => _MapScreen();
+}
+
+class _MapScreen extends State<MapScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
