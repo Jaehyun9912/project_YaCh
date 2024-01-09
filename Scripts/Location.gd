e97cf09fde453ec23c1e53b6eb91540ca7cac6a5
jaehyun9912 <limit8802@gmail.com>
2024-01-10 00:36:34 +0900

    지역 로딩 작업중

--- Scripts/Location.gd
+++ Scripts/Location.gd
@@ -1,8 +1,11 @@
 extends Area3D
 
-
+var address
+var view_manager
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	address = get_meta("name")
+	view_manager = get_node("/root/ViewManager")
 	pass # Replace with function body.
 
 
@@ -13,4 +16,5 @@ func _process(delta):
 
 func _on_location_clicked(_camera, _event, _pos, _n, _shape_idx):
 	if _event is InputEventMouseButton and _event.pressed:
-		print("Area3D가 클릭되었습니다!")
+		print("Area3D가 클릭되었습니다!" + address)
+		
--- Scripts/System/ViewManager.gd
+++ Scripts/System/ViewManager.gd
@@ -3,13 +3,15 @@ extends Node3D
 var current_location
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready():	
+	current_location = get_meta("location")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _open_location(name):
-	pass
+	
+func _open_location(address):
+	print("새 월드 로드" + address)
+	current_location.queue_free()
