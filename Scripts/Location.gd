51f6e946d60ed90a0dd3501f803be94e5f54428e
Ozeco-Mmem <open305045@gmail.com>
2024-03-25 02:19:43 +0900

    ?

--- Scripts/Location.gd
+++ Scripts/Location.gd
@@ -1,20 +1,21 @@
 extends Area3D
 
 var address
-var view_manager
+var view_manager = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	view_manager = get_node("/root/Node")
 	address = get_meta("name")
-	view_manager = get_node("/root/ViewManager")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-@warning_ignore("unused_parameter")
 func _process(delta):
 	pass
+
 
 func _on_location_clicked(_camera, _event, _pos, _n, _shape_idx):
 	if _event is InputEventMouseButton and _event.pressed:
 		print("Area3D가 클릭되었습니다!" + address)
-		
+		print(view_manager)
+		view_manager.load_world(address, "3Button")
--- Scripts/System/ViewManager.gd
+++ Scripts/System/ViewManager.gd
@@ -1,17 +1,33 @@
-extends Node3D
+extends Node
 
-var current_location
+const WORLD_PATH = "res://Worlds/"
+const PANEL_PATH = "res://InteractPanels/"
+
+var world_instance: Node = null
+var current_panel: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():	
-	current_location = get_meta("location")
+	world_instance = get_child(0)
+	current_panel = "3Button"
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func load_world(world_path: String, panel_path: String = "3Button") -> void :
+	# remove current world instance
+	if world_instance != null:
+		world_instance.queue_free()
+		
+	# load new world
+	var world_scene = load(WORLD_PATH + world_path + ".tscn")
+	print(WORLD_PATH + world_path)
+	world_instance = world_scene.instantiate()
+	add_child(world_instance)
 	
-func _open_location(address):
-	print("새 월드 로드" + address)
-	current_location.queue_free()
+	# load new Panel
+	current_panel = panel_path
