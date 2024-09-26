49282187f348c10ce50796b5d5b68ed8c286fe8b
Potan7 <whwo145@naver.com>
2024-09-26 19:51:30 +0900

    user Save Load 시스템
    user 경로에 딕셔너리를 json 파일로 저장하거나 불러올 수 있습니다.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,9 +7,22 @@ var data
 var skill : Array[Skill] = [null, null, null, null]
 
 func _ready():
+	load_player()
+		
+func save_player():
+	DataManager.save_data(data, "player")
+	
+func load_player():
 	var load_data = DataManager.load_data("player") as Dictionary
+	
 	if (load_data.is_empty()):
-		load_data = DataManager.get_data("player")
-		
-	data = load_data
-		
+		reset_player()
+	else:
+		data = load_data
+
+func reset_player():
+	var new_player = DataManager.get_data("player")
+	
+	data = new_player
+	
+	DataManager.save_data(data, "player")
--- Scripts/System/data_manager.gd
+++ Scripts/System/data_manager.gd
@@ -1,9 +1,14 @@
 extends Node
 
 const DEFAULT_PATH = "res://Data/"
+const USER_PATH = "user://"
 
+# 프로젝트의 Data 폴더에서 json 파일을 가져오는 함수
 func get_data(data_path: String) -> Dictionary:
 	var path = DEFAULT_PATH + data_path + ".json"
+	if not FileAccess.file_exists(path):
+		printerr("NoFileInPath")
+		return Dictionary()
 	var file = FileAccess.open(path, FileAccess.READ)
 	
 	var json = JSON.new()
@@ -14,8 +19,31 @@ func get_data(data_path: String) -> Dictionary:
 		print(json.get_error_message())
 		return Dictionary()
 	
+# 프로젝의 user 경로에서 json 파일을 가져오는 함수
 func load_data(data_path: String) -> Dictionary:
-	return Dictionary()
+	var path = USER_PATH + data_path + ".json"
+	if not FileAccess.file_exists(path):
+		print("NoFileInPath")
+		return Dictionary()
+		
+	var file = FileAccess.open(path, FileAccess.READ)
 	
-func save_data(save, data_path: String) -> void:
-	pass
+	var json = JSON.new()
+	var error = json.parse(file.get_as_text())
+	if error == OK:
+		return json.data
+	else:
+		print(json.get_error_message())
+		return Dictionary()
+	
+	return Dictionary() 
+	
+# 프로젝트의 user 경로에 json 파일을 저장하는 함수
+func save_data(save: Dictionary, data_path: String) -> void:
+	var path = USER_PATH + data_path + ".json"
+	var save_file = FileAccess.open(path, FileAccess.WRITE)
+	
+	var json_string = JSON.stringify(save)
+	
+	save_file.store_line(json_string)
+	
