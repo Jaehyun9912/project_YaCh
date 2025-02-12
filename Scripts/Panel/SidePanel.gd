18f8c198bc100313946b150281909b1ad9a24fab
futureots <futureots113@gmail.com>
2025-02-12 19:24:20 +0900

    side_panel_Added

--- Scripts/Panel/SidePanel.gd
+++ Scripts/Panel/SidePanel.gd
@@ -1,5 +1,35 @@
 extends Control
 
+var type:
+	get:
+		return $"ColorRect/VBoxContainer/TextType"
+var text:
+	get:
+		return $"ColorRect/VBoxContainer/Detail"
+
 enum Mode{
-	
+	Hp,
+	Budget
 }
+var mode :
+	get:
+		return mode
+	set(value):
+		mode = value
+		set_panel()
+
+func _ready():
+	pass
+
+func set_panel():
+	if mode == Mode.Hp:
+		set_hp_panel()
+	elif mode == Mode.Budget:
+		set_budget_panel()
+func set_hp_panel():
+	type.text = "현재 체력"
+	text.text = str(PlayerData.hp) + " / " + str(PlayerData.max_hp)
+
+func set_budget_panel():
+	type.text = "보유 골드"
+	text.text = "10000" + " 골드"
