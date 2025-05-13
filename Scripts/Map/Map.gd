d38963f80b2a3f8cf973cdfc8848b0bbc72da252
futureots <futureots113@gmail.com>
2025-05-13 22:52:38 +0900

    feat : move_cursor func
    location graph

--- Scripts/Map/Map.gd
+++ Scripts/Map/Map.gd
@@ -1,18 +1,47 @@
 class_name Map
 extends Node2D
 
-@export var Locations : Resource
+@export var location_resource : Resource
+
+var _cursor:
+	get:
+		return $"Cursor" as Node2D
+
+
+var current_location : MapLocation
+var locations : Array[MapLocation]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var data = DataManager.get_data("Map/TestMap")["Points"]
 	for i in data:
-		var obj = Locations.instantiate()
+		var obj = location_resource.instantiate()
+		var temp = obj as MapLocation
 		add_child(obj)
 		obj.set_location(i)
+		locations.append(temp)
 	#print(," : ", position)
-	pass # Replace with function body.
+	current_location = locations[0]
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	_cursor.position = current_location.position
+	
+
+func find_location(location : String)->MapLocation:
+	for i in locations:
+		if i.location == location:
+			return i
+	return null
+
+func move_cursor(direction : int)->void:
+	var next_location = null
+	if direction == 1:
+		next_location = find_location(current_location.up)
+	elif direction == 2:
+		next_location = find_location(current_location.right)
+	elif direction == 3:
+		next_location = find_location(current_location.down)
+	elif direction == 4:
+		next_location = find_location(current_location.left)
+	if next_location != null:
+		current_location = next_location
--- Scripts/Map/MapLocation.gd
+++ Scripts/Map/MapLocation.gd
@@ -1,17 +1,43 @@
 class_name MapLocation
 extends Node2D
 
+# 맵에 위치하는 지역 데이터
+var _location_data : Dictionary
 
-var left : MapLocation
-var right : MapLocation
-var up : MapLocation
-var down : MapLocation
+# 맵 상하좌우에 있는 지역 이름
+var left:
+	get:
+		if _location_data.has("Left"):
+			return _location_data["Left"]
+		return location
+var right:
+	get:
+		if _location_data.has("Right"):
+			return _location_data["Right"]
+		return location
+var up:
+	get:
+		if _location_data.has("Up"):
+			return _location_data["Up"]
+		return location
+var down:
+	get:
+		if _location_data.has("Down"):
+			return _location_data["Down"]
+		return location
 
-var location : String
+# 해당 지역 이름
+var location:
+	get:
+		return _location_data["Location"]
+
+
+
 func set_location(data):
-	location = data["Location"]
+	_location_data = data
 	position.x = data["xPos"]
 	position.y = data["yPos"]
+
 
 # 해당 월드로 이동
 func enter_world(viewport: Node, event: InputEvent, shape_idx: int):
