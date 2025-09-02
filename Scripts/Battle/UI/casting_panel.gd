e953a52a68d3262837125aabea7aab4a7c707a7f
Potan7 <whwo145@naver.com>
2025-09-02 12:23:42 +0900

    캐스팅 패널 기획에 맞게 변경

--- Scripts/Battle/UI/casting_panel.gd
+++ Scripts/Battle/UI/casting_panel.gd
@@ -5,34 +5,34 @@ var call: Callable
 var twn = null
 
 func _ready():
-    visible = false
+	visible = false
 
 func set_casting_panel(text, time, is_button_visible: bool, callback: Callable):
-    visible = true
-    $Label.text = text
-    $Button.visible = is_button_visible
-    
-    call = callback
-    
-    var progress = $ProgressBar as ProgressBar
-    progress.value = 0
-    
-    twn = create_tween() as Tween
-    twn.tween_property(progress, "value", 100, time)
-    twn.finished.connect(_on_end_tween)
-    
+	visible = true
+	$Label.text = text
+	$Button.visible = is_button_visible
+	
+	call = callback
+	
+	var progress = $ProgressBar as ProgressBar
+	progress.value = 0
+	
+	twn = create_tween() as Tween
+	twn.tween_property(progress, "value", 100, time)
+	twn.finished.connect(_on_end_tween)
+	
 func _on_end_tween():
-    print("tween End")
-    twn = null
-    call.call(true)
-        
-    visible = false
+	print("tween End")
+	twn = null
+	call.call(true)
+		
+	visible = false
 
 func _on_button_pressed():
-    if twn is Tween:
-        twn.kill()
-        twn = null
-    print("tween canceled")
-    call.call(false)
-        
-    visible = false
+	if twn is Tween:
+		twn.kill()
+		twn = null
+	print("tween canceled")
+	call.call(false)
+		
+	visible = false
